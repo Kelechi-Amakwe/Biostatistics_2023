1d74080d3262c440b1cb89d652d4ab4d18fe03b2
Kelechi Amakwe <122706853+Kelechi-Amakwe@users.noreply.github.com>
2023-02-21 11:16:35 -0500

    Update Jan-31.R

--- Jan-31.R
+++ Jan-31.R
@@ -2,12 +2,18 @@
 #Install external library
 if(!require(devtools)) install.packages("devtools")
 devtools::install_github("kassambara/ggpubr")
+
 #load the library
 library("ggpubr") 
+
 #Load sample dataset
 my_data <- mtcars 
+
 #plot the correlation value
 ggscatter(my_data, x = "mpg", y = "wt", add = "reg.line", conf.int = TRUE, cor.coef = TRUE, cor.method = "pearson", xlab = "Miles/(US) gallon", ylab = "Weight (1000 lbs)")
+
 #Just print the correlation value without plotting
-res <- cor.test(my_data$wt, my_data$mpg, method = "pearson") 
+res <- cor.test(my_data$wt, my_data$mpg, method = "pearson")
+
+#correlation value is = -0.87
 
